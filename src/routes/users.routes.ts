1a67cd0e0683008537fc06a4f0dd8f1e78039b8a
dandev947366 <dan.dev947366@gmail.com>
2024-06-04 11:52:04 +0300

    Nodemon

--- src/routes/users.routes.ts
+++ src/routes/users.routes.ts
@@ -7,19 +7,19 @@ const usersRouter = Router()
 usersRouter.post('/login', loginValidator, loginController)
 usersRouter.post(
     '/register', 
-    registerValidator 
-    // (req, res, next)=>{
-    //     console.log('request handler 1')
-    //     next()
-    // },
-    // (req, res, next)=>{
-    //     console.log('request handler 2')
-    //     next()
-    // },
-    // (req, res, next)=>{
-    //     console.log('request handler 3')
-    //     res.json({message:"register success"})
-    // }
+    registerValidator, 
+    (req, res, next)=>{
+        console.log('request handler 1')
+        next()
+    },
+    (req, res, next)=>{
+        console.log('request handler 2')
+        next()
+    },
+    (req, res, next)=>{
+        console.log('request handler 3')
+        res.json({message:"register success"})
+    }
     
     
     
