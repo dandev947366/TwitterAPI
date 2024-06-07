6e4a6308b2653b4cacda4453d0efef8e99cf3744
dandev947366 <dan.dev947366@gmail.com>
2024-06-07 09:27:24 +0300

    Johi validation

---
+++ src/test/README.md
@@ -0,0 +1,19 @@
+# TEST SETUP FOR TWITTER API
+
+# Packages
+Nodejs
+hapi
+bcrypt
+dotenv
+express
+express-validator
+http-errors
+jest
+jsonwebtoken
+mongoose
+morgan
+nodemon
+redis
+supertest
+
+
--- src/test/Routes/Auth.route.js
+++ src/test/Routes/Auth.route.js
@@ -3,25 +3,34 @@ const router = express.Router();
 const createError = require('http-errors');
 const User = require('../Models/User.model');
 const bcrypt = require('bcrypt'); // For password hashing
+const {authSchema} = require('../helpers/validation_schema')
+
+
+
 
 // REGISTER
 router.post('/register', async (req, res, next) => {
     try {
         const { email, password } = req.body;
+        const result = await authSchema.validateAsync(req.body)
+        console.log(result)
+        
+        
         if (!email || !password) {
             throw createError.BadRequest('Email and password are required');
         }
 
-        const doesExist = await User.findOne({ email });
+        const doesExist = await User.findOne({ email: result.email });
         if (doesExist) {
-            throw createError.Conflict(`${email} already exists`);
+            throw createError.Conflict(`${result.email} already exists`);
         }
 
         const hashedPassword = await bcrypt.hash(password, 10); // Hash the password
-        const user = new User({ email, password: hashedPassword });
+        const user = new User(result);
         const savedUser = await user.save();
         res.status(201).send(savedUser);
     } catch (error) {
+    if(error.isJoi){error.status=422}
         next(error);
     }
 });
