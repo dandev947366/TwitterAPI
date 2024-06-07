a416d92d53e82813659f2d43f2ac3216282a39aa
dandev947366 <dan.dev947366@gmail.com>
2024-06-07 09:50:08 +0300

    Bcrypt added

--- src/test/Models/User.model.js
+++ src/test/Models/User.model.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
+const bcrypt = require('bcrypt'); // Corrected typo
 
 main().catch(err => console.log(err));
 
@@ -8,7 +9,7 @@ async function main() {
     await mongoose.connect('mongodb://localhost:27017/yourdbname', {
         useNewUrlParser: true,
         useUnifiedTopology: true,
-        useCreateIndex: true,
+        // useCreateIndex is not needed in Mongoose 6.x
     });
 }
 
@@ -23,6 +24,25 @@ const UserSchema = new Schema({
         type: String,
         required: true
     }
+});
+
+// Pre-save hook to hash the password before saving
+UserSchema.pre('save', async function (next) {
+    try {
+        if (this.isModified('password') || this.isNew) {
+            const salt = await bcrypt.genSalt(10);
+            this.password = await bcrypt.hash(this.password, salt);
+        }
+        next();
+    } catch (error) {
+        next(error);
+    }
+});
+
+// Post-save hook
+UserSchema.post('save', function (doc, next) {
+    console.log('User saved:', doc.email);
+    next();
 });
 
 const User = mongoose.model('User', UserSchema);
